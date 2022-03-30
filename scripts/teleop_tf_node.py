809eebb9e8009b0834792a4504198ef2dec7e2d3
Chris E. Mower <chris.mower@ed.ac.uk>
2022-03-30 21:06:15 +0100

    Use custom_ros_tools and add reset_zero transform and other updates

--- scripts/teleop_tf_node.py
+++ scripts/teleop_tf_node.py
@@ -23,25 +23,27 @@ import rospy
 import numpy as np
 import tf_conversions
 from std_msgs.msg import Float64MultiArray
-from rpbi.tf_interface import TfInterface
+from custom_ros_tools.tf import TfInterface
 from custom_srvs.srv import SetTransform, SetTransformResponse
-from custom_srvs.custom_srvs import ToggleService
+from custom_ros_tools.ros_comm import ToggleService
+from std_srvs.srv import Trigger, TriggerResponse
 
 class Node:
 
     def __init__(self):
         rospy.init_node('teleop_tf_node')
         self.tf = TfInterface()
-        self.parent_frame = rospy.get_param('~parent_frame', 'world')
-        self.child_frame = rospy.get_param('~child_frame')
+        self.parent_frame = rospy.get_param('~parent_frame_id', 'world')
+        self.child_frame = rospy.get_param('~child_frame_id')
         hz = rospy.get_param('~hz', 100)
         self.dt = 1.0/float(hz)
         self.h = np.zeros(6)
         self.transform = np.zeros(6)  # position + euler angles
         self.timer = None
-        self.sub = None
+        rospy.Subscriber('operator_node/signal', Float64MultiArray, self.callback)
         ToggleService('toggle_teleop_tf', self.start_teleop, self.stop_teleop)
         rospy.Service('reset_transform', SetTransform, self.reset_transform)
+        rospy.Service('reset_zero', Trigger, self.reset_zero)
         if rospy.get_param('~start_on_init', False):
             self.start_teleop()
 
@@ -51,9 +53,12 @@ class Node:
         self.transform[3:] = tf_conversions.transformations.euler_from_quaternion(rot)
         return SetTransformResponse(success=True, message='reset transform to ' + str(self.transform))
 
+    def reset_zero(self, req):
+        self.transform = np.zeros(6)
+        return TriggerResponse(success=True, message='reset transform to zero')
+
     def start_teleop(self):
-        if (self.sub is None) and (self.timer is None):
-            self.sub = rospy.Subscriber('operator_node/signal', Float64MultiArray, self.callback)
+        if self.timer is None:
             self.timer = rospy.Timer(rospy.Duration(self.dt), self.main_loop)
             success = True
             message = 'started tf teleoperation node'
@@ -64,15 +69,14 @@ class Node:
         return success, message
 
     def stop_teleop(self):
-        if (self.sub is not None) and (self.timer is not None):
-            self.sub.unregister()
+        if self.timer is not None:
             self.h = np.zeros(6)  # ensure no more motion
             self.timer.shutdown()
             success = True
             message = 'stopped tf teleoperation node'
         else:
             success = False
-            message = 'user attempted to stop ttf teleoperation node, but it is not running!'
+            message = 'user attempted to stop tf teleoperation node, but it is not running!'
         return success, message
 
     def callback(self, msg):
