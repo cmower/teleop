906e96afa808622cf98ea5dc8b06ed41750c2c94
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2022-05-23 06:15:29 +0100

    Ensure origin is in limits

--- scripts/teleop_tf_node.py
+++ scripts/teleop_tf_node.py
@@ -19,6 +19,7 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+import sys
 import rospy
 import numpy as np
 import tf_conversions
@@ -48,6 +49,9 @@ class Node:
         zlim = config.get('zlim', [-np.inf, np.inf])
         self.lolim = np.array([xlim[0], ylim[0], zlim[0]])
         self.uplim = np.array([xlim[1], ylim[1], zlim[1]])
+        if not self.is_in_limit([0,0,0]):
+            rospy.logerr('origin is not inside the specified box limits!')
+            sys.exit(0)
 
         # Setup other class attributes
         self.h = np.zeros(6)
